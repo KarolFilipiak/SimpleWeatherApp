a2c478001b4ac858ce2c7c7c151fb9bdf81e0e26
Karol <karfil3@st.amu.edu.pl>
2022-07-05 10:49:24 +0200

    2.2.1 - added provider to main and fixed homepage

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:weather/home_page.dart';
 import 'views/welcome_page.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weather/sharedpreferences/shared_city_list.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
  void main()  {
-  runApp(MyApp(
+  runApp(
     ChangeNotifierProvider<CitiesListModel>(
       create: (_) => CitiesListModel(),
       lazy: false,
       child: const MyApp(),
     )
-  ));
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp(ChangeNotifierProvider<CitiesListModel> changeNotifierProvider, {Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FutureProvider(
-      create: ((_) => SharedPreferences.getInstance()),
-      lazy: false,
-      initialData: null,
-      child: MaterialApp(
+    return MaterialApp(
           title: 'Weather app',
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
             primarySwatch: Colors.blue,
             scaffoldBackgroundColor: Color.fromARGB(255, 94, 3, 168),
           ),
-          home: HomePage(),
-        ),
+          home: const HomePage(),
+        );
 
-    );
+ 
   }
 }
